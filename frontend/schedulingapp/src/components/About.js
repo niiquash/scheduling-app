2d594d06ee27a931f95acf063caf9120fdf34551
niiquash <qua16006@byui.edu>
2022-11-30 10:50:21 -0700

    finished about page

--- frontend/schedulingapp/src/components/About.js
+++ frontend/schedulingapp/src/components/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
-        <main>
-            <h1>About</h1>
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>
+                This is an appointment scheduling app for individuals who need timely healthcare needs met. Contact the creator of this app via email at <Link to="#">atquarsh@outlook.com</Link>
+            </p>
         </main>
     )
 }
